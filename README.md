ec944298b7bda3810d075458beb8b286f4aa4994
Henning <github@skotax.de>
2018-09-05 10:51:42 +0200

    change description and add version number

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # stm32_crc_tool
-Calculate a CRC checksum as implemented in the STM32F2 and STM32F4 hardware (On these controllers the CRC-Module cannot be configured). Pass the data as command line argument to this program to calculate the CRC checksum with 32 bits with the same algorithm as on the STM32Fx modules.
+Calculate a CRC checksum as implemented in the STM32Fx Controllers without Crypto-Hash module (e.g.: F405, F407, F427, F469, etc.). On these controllers the CRC-Module cannot be configured.
+
+Pass the data as command line argument to this program to calculate the CRC checksum with 32 bits with the same algorithm as on the STM32Fx modules.
  
- ## usage
-    usage: stm32 crc tool [-h] [-d] [-b BITS] [-p POLY] [-i VALUE] [data [data ...]]
- ### positional arguments:
+## usage
+    usage: stm32_crc_tool [-h] [-v] [-d] [-b BITS] [-p POLY] [-i VAL]
+                          [data [data ...]]
+### positional arguments:
     data                  list of characters that will be treated as numbers
 
 ### optional arguments:
     -h, --help            show this help message and exit
+    -v, --version         show program's version number and exit
     -d, --demo            display a demonstration of the stm32f4 crc algorithm
-    -b BITS, --bits BITS  specify the bit length of the checksum (default is 32
-                          bits)
-    -p POLY, --poly POLY  specify the crc polynome (default is 0x04C11DB7)
-    -i VALUE, --init VALUE
-                          specify the initial crc value (default is 0xFFFFFFFF)
+    -b BITS, --bits BITS  bit length of the checksum (default is 32 bits)
+    -p POLY, --poly POLY  crc polynome (default is 0x04C11DB7)
+    -i VAL, --init VAL    initial crc value (default is 0xFFFFFFFF)
 
 The default values equal the ones embedded in the STM hardware modules.
--- stm32_crc_tool.py
+++ stm32_crc_tool.py
@@ -94,25 +94,28 @@ def demonstration():
 
 
 def handle_arguments():
-    parser = argparse.ArgumentParser(description="Calculate a CRC checksum as implemented in the STM32F2 and "
-                                                 "STM32F4 hardware (On these controllers the CRC-Module cannot "
-                                                 "be configured). Pass the data as command line argument to "
-                                                 "this program to calculate the CRC checksum with 32 bits with "
-                                                 "the same algorithm as on the STM32Fx modules.", 
-                                     prog="stm32 crc tool",
+    parser = argparse.ArgumentParser(description="Calculate a CRC checksum as implemented in the STM32Fx "
+                                                 "Controllers without Crypto-Hash module (e.g.: F405, F407, "
+                                                 "F427, F469, etc.). On these controllers the CRC-Module cannot "
+                                                 "be configured. " 
+                                                 "Pass the data as command line argument to this program to "
+                                                 "calculate the CRC checksum with 32 bits with the same "
+                                                 "algorithm as on the STM32Fx modules.",
+                                     prog="stm32_crc_tool",
                                      epilog="The default values equal the ones embedded in the STM hardware "
                                             "modules.")
 
     parser.add_argument("data", nargs='*',
                         help="list of characters that will be treated as numbers")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.0")
     parser.add_argument("-d", "--demo", action="store_true", 
                         help="display a demonstration of the stm32f4 crc algorithm")
     parser.add_argument("-b", "--bits", type=int, metavar="BITS", default=32, 
-                        help="specify the bit length of the checksum (default is 32 bits)")
+                        help="bit length of the checksum (default is 32 bits)")
     parser.add_argument("-p", "--poly", type=int, default=0x04C11DB7, 
-                        help="specify the crc polynome (default is 0x04C11DB7)")
-    parser.add_argument("-i", "--init", type=int, metavar="VALUE", default=0xFFFFFFFF, 
-                        help="specify the initial crc value (default is 0xFFFFFFFF)")
+                        help="crc polynome (default is 0x04C11DB7)")
+    parser.add_argument("-i", "--init", type=int, metavar="VAL", default=0xFFFFFFFF, 
+                        help="initial crc value (default is 0xFFFFFFFF)")
 
     return parser.parse_args(), parser
 
