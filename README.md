37e0c1e35545672178eb013f8eee194339e511cc
Henning <github@skotax.de>
2018-11-14 12:22:53 +0100

    implement crc calculation of a binary file and update documentation

--- README.md
+++ README.md
@@ -1,24 +1,128 @@
 # stm32_crc_tool
-Calculate a CRC checksum as implemented in the STM32Fx Controllers without Crypto-Hash module (e.g.: F405, F407, F427, F469, etc.). On these controllers the CRC-Module cannot be configured.
+Calculate a CRC checksum as implemented in the STM32Fx Controllers without Crypto-Hash module (e.g.: F405, F407, F427, 
+F469, etc.). On these controllers the CRC-Module cannot be configured.
 
-Pass the data as command line argument to this program to calculate the CRC checksum with 32 bits with the same algorithm as on the STM32Fx modules.
+Pass the data as command line argument to this program to calculate the CRC checksum with 32 bits with the same 
+algorithm as on the STM32Fx modules.
  
-## usage
-    usage: stm32_crc_tool [-h] [-v] [-d] [-b BITS] [-p POLY] [-i VAL]
+## Usage
+    usage: stm32_crc_tool [-h] [-v] [--verbose] [-d] [-b BITS] [-p POLY] [-i VAL]
+                          [-f PATH]
                           [data [data ...]]
-### positional arguments:
+### Positional arguments:
     data                  list of characters that will be treated as numbers
 
-### optional arguments:
+### Optional arguments:
     -h, --help            show this help message and exit
     -v, --version         show program's version number and exit
+    --verbose             enables additional debug output
     -d, --demo            display a demonstration of the stm32f4 crc algorithm
     -b BITS, --bits BITS  bit length of the checksum (default is 32 bits)
-    -p POLY, --poly POLY  crc polynome (default is 0x04C11DB7)
+    -p POLY, --poly POLY  crc polynomial (default is 0x04C11DB7)
     -i VAL, --init VAL    initial crc value (default is 0xFFFFFFFF)
+    -f PATH, --file PATH  use a file as input. The file will be parsed in 4 byte
+                          chunks.
 
 The default values equal the ones embedded in the STM hardware modules.
 
-# Licence
+### Example
+Linux:
 
+    user@machine: $ python3 stm32_crc_tool.py -f stm32_crc_tool.py -b 8 -p 171 -i 00
+    0xb6
+
+## Library usage
+The stm32_crc_tool can also be used as a library for other python programs. Therefore the stm32_crc_tool python file 
+needs to be included. Then these functions are available: 
+
+### embedded_crc
+```python
+embedded_crc(input_data:int, poly:int=79764919, initial_crc:int=4294967295, sizeof_input:int=32) -> int
+```
+Calculates the crc for one given integer. This function represents one iteration on the embedded crc peripheral
+in the STM controller family. (The parameter default values equal the ones embedded in the STM hardware modules.)
+    
+_rtype_ `int`
+
+_param_ input_data: one integer value with the specified bit length
+
+_param_ poly: crc polynomial to use in the algorithm
+
+_param_ initial_crc: crc value to start with (e.g. value from the previous iteration)
+
+_param_ sizeof_input: bit length of the desired crc value
+
+_return_ crc value of the given integer
+
+### msb
+```python
+msb(val:int, bits:int=8) -> bool
+```
+returns whether the Most Significant Bit (msb) of the provided value is 1
+
+_rtype_ bool
+
+_param_ **val**: numeric value to check the most msb
+
+_param_ **bits**: bit length of the value
+
+_return_ true if the msb is a 1, false otherwise
+
+### prepare_data
+```python
+prepare_data(data:str) -> [<class 'int'>]
+```
+Convert a string into a list of integers. (Uses utf-8 encoding on most machines.)
+
+_rtype_ [int]
+
+_param_ **data**: string to convert
+
+_return_ list of the int representations of the  chars from the string
+
+### process_file
+```python
+process_file(filename:str, poly:int=79764919, initial_crc:int=4294967295, sizeof_input:int=32) -> int
+```
+Calculates the crc for a file, interpreted as binary data. (The parameter default values equal the ones embedded
+in the STM hardware modules.)
+
+_rtype_ int
+
+_param_ **filename**: path to the file
+
+_param_ **poly**: crc polynomial to use in the algorithm
+
+_param_ **initial_crc**: crc value to start with
+
+_param_ **sizeof_input**: bit length of the desired crc value
+
+_return_ crc value of the provided file
+
+### process_queue
+```python
+process_queue(data:str, poly:int=79764919, initial_crc:int=4294967295, sizeof_input:int=32) -> int
+```
+Calculates the crc for a string. (The parameter default values equal the ones embedded in the STM hardware
+modules.)
+
+_rtype_ int
+
+_param_ **data**: the crc value is calculated for this string
+
+_param_ **poly**: crc polynomial to use in the algorithm
+
+_param_ **initial_crc**: crc value to start with
+
+_param_ **sizeof_input**: bit length of the desired crc value
+
+_return_ crc value of the provided string
+
+## Licence
 This software is provided under the [MIT licence](LICENCE). 
+
+
+## Future work
+My personal plans for this repository:
+- Improve the performance of the file crc calculation
+- Add CI
--- stm32_crc_tool.py
+++ stm32_crc_tool.py
@@ -1,56 +1,86 @@
 #!/usr/bin/python3
 
+from struct import unpack
+
 _debug_mode = False
 
 
-def debug_msg(a, b):
-    if(_debug_mode):
+def _debug_msg(a, b) -> None:
+    if _debug_mode:
         print(a, b)
 
 
-def msb(val:int, bits: int=8)-> bool:
+def msb(val: int, bits: int = 8) -> bool:
     """returns whether the Most Significant Bit (msb) of the provided value is 1
-       (only for 32 bit variables)"""
-    return bool(val & (1 << (bits-1)))
+    :rtype: bool
+    :param val: numeric value to check the most msb
+    :param bits: bit length of the value
+    :return: true if the msb is a 1, false otherwise
+    """
+    return bool(val & (1 << (bits - 1)))
 
 
-def embedded_crc(input_data: int, poly: int=0x04C11DB7, initial_crc: int=0xFFFFFFFF, sizeof_input: int=32) -> int:
+def embedded_crc(input_data: int, poly: int = 0x04C11DB7, initial_crc: int = 0xFFFFFFFF, sizeof_input: int = 32) -> int:
+    """Calculates the crc for one given integer. This function represents one iteration on the embedded crc peripheral
+    in the STM controller family. (The parameter default values equal the ones embedded in the STM hardware modules.)
+    :rtype: int
+    :param input_data: one integer value with the specified bit length
+    :param poly: crc polynomial to use in the algorithm
+    :param initial_crc: crc value to start with (e.g. value from the previous iteration)
+    :param sizeof_input: bit length of the desired crc value
+    :return: crc value of the given integer
+    """
     # create the proper mask
     msk = 0
     for i in range(sizeof_input):
         msk = (msk << 1) | 1
 
     # start of the algorithm described in the stm32 crc application manual
-    debug_msg("initial_crc", bin(initial_crc))
-    debug_msg("input_data", bin(input_data))
+    _debug_msg("initial_crc", bin(initial_crc))
+    _debug_msg("input_data", bin(input_data))
 
     crc = initial_crc ^ input_data
-    debug_msg("crc = initial ^ input", bin(crc))
-    
-    bindex = 0
-    while bindex < sizeof_input:
+    _debug_msg("crc = initial ^ input", bin(crc))
 
-        debug_msg("crc", bin(crc))
+    b_index = 0
+    while b_index < sizeof_input:
+
+        _debug_msg("crc", bin(crc))
 
         if msb(crc, sizeof_input):
             crc = ((crc << 1) ^ poly) & msk
-            debug_msg("crc ^ poly", bin(crc))
+            _debug_msg("crc ^ poly", bin(crc))
         else:
             crc = (crc << 1) & msk
 
-        bindex += 1
+        b_index += 1
 
     return crc
 
 
-def prepare_data(data):
+def prepare_data(data: str) -> [int]:
+    """Convert a string into a list of integers. (Uses utf-8 encoding on most machines.)
+    :rtype: [int]
+    :param data: string to convert
+    :return: list of the int representations of the  chars from the string
+    """
     reduced_data = []
     for word in data:
         for char in word:
             reduced_data.append(ord(char))
     return reduced_data
 
-def process_queue(data, poly: int=0x04C11DB7, initial_crc: int=0xFFFFFFFF, sizeof_input: int=32) -> int:
+
+def process_queue(data: str, poly: int = 0x04C11DB7, initial_crc: int = 0xFFFFFFFF, sizeof_input: int = 32) -> int:
+    """Calculates the crc for a string. (The parameter default values equal the ones embedded in the STM hardware
+    modules.)
+    :rtype: int
+    :param data: the crc value is calculated for this string
+    :param poly: crc polynomial to use in the algorithm
+    :param initial_crc: crc value to start with
+    :param sizeof_input: bit length of the desired crc value
+    :return: crc value of the provided string
+    """
     data = prepare_data(data)
     crc = initial_crc
     for val in data:
@@ -58,7 +88,41 @@ def process_queue(data, poly: int=0x04C11DB7, initial_crc: int=0xFFFFFFFF, sizeo
     return crc
 
 
-def demonstration():
+def process_file(filename: str, poly: int = 0x04C11DB7, initial_crc: int = 0xFFFFFFFF, sizeof_input: int = 32) -> int:
+    """Calculates the crc for a file, interpreted as binary data. (The parameter default values equal the ones embedded
+    in the STM hardware modules.)
+    :rtype: int
+    :param filename: path to the file
+    :param poly: crc polynomial to use in the algorithm
+    :param initial_crc: crc value to start with
+    :param sizeof_input: bit length of the desired crc value
+    :return: crc value of the provided file
+    """
+    crc = initial_crc
+    with open(filename, "rb") as input_file:
+        binary = input_file.read()
+
+        for i in range(len(binary) // 4):
+            chunk = binary[i * 4: i * 4 + 4]
+            _debug_msg("chunk", chunk)
+            val = unpack('i', chunk)[0]
+            _debug_msg("unpacked value", val)
+            crc = embedded_crc(val, poly, crc, sizeof_input)
+
+        remaining_bytes = len(binary) % 4
+        if remaining_bytes > 0:
+            chunk = binary[-remaining_bytes:] + bytes(4 - remaining_bytes)
+            _debug_msg("chunk", chunk)
+            val = unpack('i', chunk)[0]
+            _debug_msg("unpacked value", val)
+            crc = embedded_crc(val, poly, crc, sizeof_input)
+
+    return crc
+
+
+def _demonstration() -> None:
+    """Prints a demonstration of the stm32 crc algorithm. The first example matches the example from the stm32f4
+    app note: AN4187. The second shows uses 32 bits as implemented in the stm32f4 hardware"""
     global _debug_mode
     print("stm - crc tool demonstration\n")
 
@@ -93,11 +157,12 @@ def demonstration():
     print("computed crc =", hex(crc))
 
 
-def handle_arguments():
+def _handle_arguments() -> (object, object):
+    import argparse
     parser = argparse.ArgumentParser(description="Calculate a CRC checksum as implemented in the STM32Fx "
                                                  "Controllers without Crypto-Hash module (e.g.: F405, F407, "
                                                  "F427, F469, etc.). On these controllers the CRC-Module cannot "
-                                                 "be configured. " 
+                                                 "be configured. "
                                                  "Pass the data as command line argument to this program to "
                                                  "calculate the CRC checksum with 32 bits with the same "
                                                  "algorithm as on the STM32Fx modules.",
@@ -107,29 +172,38 @@ def handle_arguments():
 
     parser.add_argument("data", nargs='*',
                         help="list of characters that will be treated as numbers")
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.0")
-    parser.add_argument("-d", "--demo", action="store_true", 
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.1")
+    parser.add_argument("--verbose", action="store_true",
+                        help="enables additional debug output")
+    parser.add_argument("-d", "--demo", action="store_true",
                         help="display a demonstration of the stm32f4 crc algorithm")
-    parser.add_argument("-b", "--bits", type=int, metavar="BITS", default=32, 
+    parser.add_argument("-b", "--bits", type=int, metavar="BITS", default=32,
                         help="bit length of the checksum (default is 32 bits)")
-    parser.add_argument("-p", "--poly", type=int, default=0x04C11DB7, 
-                        help="crc polynome (default is 0x04C11DB7)")
-    parser.add_argument("-i", "--init", type=int, metavar="VAL", default=0xFFFFFFFF, 
+    parser.add_argument("-p", "--poly", type=int, default=0x04C11DB7,
+                        help="crc polynomial (default is 0x04C11DB7)")
+    parser.add_argument("-i", "--init", type=int, metavar="VAL", default=0xFFFFFFFF,
                         help="initial crc value (default is 0xFFFFFFFF)")
+    parser.add_argument("-f", "--file", type=str, metavar="PATH",
+                        help="use a file as input. The file will be parsed in 4 byte chunks.")
 
     return parser.parse_args(), parser
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # read command line arguments
-    import argparse
-    args, parser = handle_arguments()
+    args, argument_parser = _handle_arguments()
 
     if args.demo:
-        demonstration()
+        _demonstration()
     else:
-        if len(args.data) == 0:
-            parser.print_help()
+        if args.verbose:
+            _debug_mode = True
+
+        if args.file is not None:
+            CRC = process_file(args.file, args.poly, args.init, args.bits)
+            print(hex(CRC))
+        elif len(args.data) == 0:
+            argument_parser.print_help()
         else:
-            crc = process_queue(args.data, args.poly, args.init, args.bits)
-            print(hex(crc))
+            CRC = process_queue(args.data, args.poly, args.init, args.bits)
+            print(hex(CRC))
